c542b177ff16abe3435519c2e5c55f4b60dfb6ab
Ben Pearce <ben.pearce@octopus.com>
2022-10-01 15:47:19 +1000

    remove var block

--- variables/var-framework.go
+++ variables/var-framework.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-	. "fmt"
+	"fmt"
 	"reflect"
 	"strconv"
 )
 
-var (
-	name, course = "Ben", "course name"
-	module       = "4"
-	clip         = 2
-)
-
 func main() {
+	name := "Ben"
+	course := "course name"
+	module := "4"
+	clip := 2
 
-	Println("Name and course are", name, "and", course, ".")
-	Println("Module and clip are", module, "and", clip, ".")
-	Println("Name is type ", reflect.TypeOf(name))
-	Println("Module is type", reflect.TypeOf(module))
+	fmt.Println("Name and course are", name, "and", course, ".")
+	fmt.Println("Module and clip are", module, "and", clip, ".")
+	fmt.Println("Name is type ", reflect.TypeOf(name))
+	fmt.Println("Module is type", reflect.TypeOf(module))
 	iModule, err := strconv.Atoi(module)
 	if err == nil {
 		total := iModule + clip
-		Println("module plus clip is", total)
+		fmt.Println("module plus clip is", total)
 	}
 }
