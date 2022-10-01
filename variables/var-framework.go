6d4a51d6b11c4f13e93da8462bc2099ef8aae4e9
Ben Pearce <ben.pearce@octopus.com>
2022-10-01 15:49:10 +1000

    func level variables must be used

--- variables/var-framework.go
+++ variables/var-framework.go
@@ -11,6 +11,7 @@ func main() {
 	course := "course name"
 	module := "4"
 	clip := 2
+	//courseComplete := false
 
 	fmt.Println("Name and course are", name, "and", course, ".")
 	fmt.Println("Module and clip are", module, "and", clip, ".")
